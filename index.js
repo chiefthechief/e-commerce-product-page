bc187f90008eeaf1ac950161508e485c06bf9468
cujo <cujothechief@gmail.com>
2022-08-15 07:33:40 +0000

    finally got slider to work!

---
+++ index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper", {
+   loop: true,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+   }
+})
