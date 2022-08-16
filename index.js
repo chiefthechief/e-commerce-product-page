43701c4b7af9359513ba20dc6dcd290021a51898
cujo <cujothechief@gmail.com>
2022-08-16 16:32:51 +0000

    almost done with the mobile view

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//ELEMENTS TO BE USED
+let body = document.querySelector('body');
+let backgroundDiv = document.querySelector('body > div:first-child');
+let menu = document.querySelector('header nav.nav-left ul');
+let menuBar = document.querySelector('header nav.nav-left i:first-child');
+let closeMenu = document.querySelector('header nav.nav-left ul li i');
+//SLIDER
 var swiper = new Swiper(".swiper", {
    loop: true,
    navigation: {
@@ -7,4 +14,14 @@ var swiper = new Swiper(".swiper", {
    pagination: {
       el: ".swiper-pagination",
    }
+})
+menuBar.addEventListener('click', () => {
+   menu.classList.add('slide');
+   body.classList.add('modal');
+   backgroundDiv.classList.add('overlay');
+})
+closeMenu.addEventListener('click', () => {
+   menu.classList.remove('slide');
+   body.classList.remove('modal');
+   backgroundDiv.classList.remove('overlay');
 })
