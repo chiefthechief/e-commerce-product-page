c12b408323e2be457097d979920de4aca7594510
cujo <cujothechief@gmail.com>
2022-08-17 13:18:00 +0000

    done with header desktop version

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //ELEMENTS TO BE USED
-let body = document.querySelector('body');
-let backgroundDiv = document.querySelector('body > div:first-child');
 let menu = document.querySelector('header nav.nav-left ul');
 let menuBar = document.querySelector('header nav.nav-left i:first-child');
 let closeMenu = document.querySelector('header nav.nav-left ul li i');
+let main = document.querySelector('main');
 //SLIDER
 var swiper = new Swiper(".swiper", {
    loop: true,
@@ -15,13 +14,16 @@ var swiper = new Swiper(".swiper", {
       el: ".swiper-pagination",
    }
 })
+//DYNAMICALLY ADDING STYLES
 menuBar.addEventListener('click', () => {
    menu.classList.add('slide');
    body.classList.add('modal');
-   backgroundDiv.classList.add('overlay');
 })
 closeMenu.addEventListener('click', () => {
    menu.classList.remove('slide');
    body.classList.remove('modal');
-   backgroundDiv.classList.remove('overlay');
 })
+if(window.innerWidth > 499){
+   main.classList.add('flex');
+   menu.classList.add('flex');
+}
