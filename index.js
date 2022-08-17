00c5396fa16790c9ab1660b05f6711cd1dd521f5
cujo <cujothechief@gmail.com>
2022-08-17 20:47:50 +0000

    think I'm done with the desktop view

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ let menu = document.querySelector('header nav.nav-left ul');
 let menuBar = document.querySelector('header nav.nav-left i:first-child');
 let closeMenu = document.querySelector('header nav.nav-left ul li i');
 let main = document.querySelector('main');
+let imageBanner = document.querySelector('main div.image-banner');
+let img = document.createElement('img');
+let bannerImages = document.querySelectorAll('main > div.container > div.swiper > div.swiper-wrapper > div.swiper-slide > div.image-wrapper > img');
 //SLIDER
 var swiper = new Swiper(".swiper", {
-   loop: true,
+   loop: false,
    navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
    },
    pagination: {
       el: ".swiper-pagination",
-   }
+   },
 })
 //DYNAMICALLY ADDING STYLES
 menuBar.addEventListener('click', () => {
@@ -27,3 +30,10 @@ if(window.innerWidth > 499){
    main.classList.add('flex');
    menu.classList.add('flex');
 }
+img.src = './images/image-product-1.jpg';
+imageBanner.appendChild(img);
+bannerImages.forEach(element => {
+   element.addEventListener('click', () => {
+   img.setAttribute('src', element.getAttribute('src'))
+   })
+})
